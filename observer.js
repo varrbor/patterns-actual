92081efa847b40c5781b48d48e1538225bf10100
varrb <varrbor@ukr.net>
2021-09-18 21:49:04 +0300

    Observer added

---
+++ observer.js
@@ -0,0 +1,34 @@
+class AutoNews {
+    constructor() {
+        this.news = "";
+        this.actions = [];
+    }
+    setNews(text) {
+        this.news = text;
+        this.notifyAll();
+    }
+    notifyAll() {
+        return this.actions.forEach((subs) => subs.inform(this));
+    }
+    register(observer) {
+        return this.actions.push(observer);
+    }
+    unregister(observer) {
+        this.actions = this.actions.filter((el) => !el instanceof observer);
+    }
+}
+class Jack {
+    inform(message) {
+        console.log(`Jack has been informed about: ${message.news}`);
+    }
+}
+class Max {
+    inform(message) {
+        console.log(`Max has been informed about: ${message.news}`);
+    }
+}
+const autoNews = new AutoNews();
+autoNews.register(new Jack());
+autoNews.register(new Max());
+
+console.log(autoNews.setNews("New tesla price is 40000"));
