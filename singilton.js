b3ba84f6c4b0a4c194c9f23f052dce33edef29aa
bvarv <bvarv@softserveinc.com>
2021-12-04 11:30:41 +0200

    qqqq

--- singilton.js
+++ singilton.js
@@ -1,26 +1,16 @@
-class Database {
-    constructor(data) {
-        if (Database.exists) {
-            return Database.instance;
-        }
-        this._data = data;
-        Database.instance = this;
-        Database.exists = true;
-        return this;
+var Singleton = (function() {
+    var instance;
+    function createInstance(){
+        return new Object("I'm am an instance");
     }
+    return {
+        getInstance: function(){
+            if(!instance){
+                instance= createInstance()
+            }
+            return instance
+        }}
+})();
 
-    getData() {
-        return this._data;
-    }
-
-    setData(data) {
-        this._data = data;
-    }
-}
-
-// usage
-const mongo = new Database('mongo');
-console.log(mongo.getData()); // mongo
-
-const mysql = new Database('mysql');
-console.log(mysql.getData()); // mongo
+console.log(Singleton.getInstance())
+console.log(Singleton.getInstance())
